2bdfbebbc3f8004124a3b0a24988327cbe37e2fb
aritra43 <mukhopadhyayaritra7@gmail.com>
2024-07-09 19:26:33 +0530

    TailwindCSS Styling Done

--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,7 @@ const[fileUpload,setFileUpload]= useState([])
     await updateDoc(movieDoc,{title :updatedTitle});
   };
   //Delete Movie
-const DeleteMovie=async(id) =>{
+const deleteMovie=async(id) =>{
   const movieDoc=doc(db,"movies",id);
   await deleteDoc(movieDoc);
 };
@@ -82,26 +82,35 @@ const DeleteMovie=async(id) =>{
 
 
   return (
+    <div className="w-full h-72 pt-5 border-2 border-solid border-white mt-3">
     <div className="App">
       <Auth />
-      <div >
-        <input placeholder="Movie title"
-          onChange={(e) => setNewMovieTitle(e.target.value)}
-        />
-        <input placeholder="release date" type="number"
-          onChange={(e) => setNewReleaseDate(Number(e.target.value))}
-        />
-        <input type="checkbox" checked={isNewMovieOscar} onChange={(e) =>setIsNewMovieOscar(e.target.checked)}/>
-        <label>Recieved an Oscar</label>
-        <button onClick={onSubmitMovie}>Submit Movie</button>
       </div>
+
+      <div className="w-full h-auto border-2 border-solid border-white mt-16">
+      <div >
+        <div className="mt-5 mb-6 w-1/2 border-2 border-solid border-white m-auto rounded-full">
+        <input className="m-auto p-3 w-56 bg-transparent border-none outline-none placeholder-white" placeholder="Movie title"
+          onChange={(e) => setNewMovieTitle(e.target.value)}
+        /></div>
+        <div className="mt-5 mb-6 w-1/2 border-2 border-solid border-white m-auto rounded-full">
+        <input className="m-auto p-3 w-56 bg-transparent border-none outline-none placeholder-white" placeholder="Release Date" type="number"
+          onChange={(e) => setNewReleaseDate(Number(e.target.value))}
+        /></div>
+        <div className="w-full">
+        <input className="ml-10" type="checkbox" checked={isNewMovieOscar} onChange={(e) =>setIsNewMovieOscar(e.target.checked)}/>
+        <label className="text-white mr-5">Recieved an Oscar</label>
+        <button className="border-2 ml-10border-solid border-white rounded-full p-2 pl-4 pr-4 text-white hover:text-black hover:bg-gradient-to-r from-slate-100 to-slate-400" onClick={onSubmitMovie}>Submit Movie</button>
+        </div>
+      </div>
+
       
       <div>
         {movieList.map((movie) => (
         <div>
           <h1 style={{color:movie.receivedAnOscar?"green":"red"}}>{movie.title}</h1>
           <p>Date : {movie.releaseDate}</p>
-          <button onClick={() => DeleteMovie(movie.id)}>Delete Movie</button>
+          <button onClick={() => deleteMovie(movie.id)}>Delete Movie</button>
 
           <input placeholder="new Title" onChange={(e) => setUpdatedTitle(e.target.value)}></input>
           <button onClick={() => UpdateMovieTitle(movie.id)}>Update title</button>
@@ -111,13 +120,13 @@ const DeleteMovie=async(id) =>{
       </div>
 
       <div>
-        <input type="file" onChange={(e) => setFileUpload(e.target.files[0])}/>
-        <button onClick={uploadFile}>Upload File</button>
+        <input className="text-white ml-2" type="file" onChange={(e) => setFileUpload(e.target.files[0])}/>
+        <button className="border-2 ml-10border-solid border-white rounded-full p-2 pl-4 pr-4 text-white hover:text-black hover:bg-gradient-to-r from-slate-100 to-slate-400 mt-2 mr-5 mb-2" onClick={uploadFile}>Upload File</button>
       </div>
       <deleteMovie/> 
 
     </div>
-
+    </div>
       
    
   ); //closing return 
--- src/Components/auth.jsx
+++ src/Components/auth.jsx
@@ -36,11 +36,17 @@ export const Auth=() =>
     };
     return (
     <>
-        <input placeholder="Email" onChange={(e) => setEmail(e.target.value)}/>
-        <input placeholder="Password" type="password" onChange={(e) => setPassword(e.target.value)}/>
-        <button onClick={signIn}>Sign In</button>
-        <button onClick={signInWithGoogle}>Sign In with google</button>
-        <button onClick={Logout}>Logout</button>
+        <div className="mt-5 mb-6 w-1/2 border-2 border-solid border-white m-auto rounded-full">
+        <input className="block m-auto p-3 w-56 bg-transparent border-none outline-none placeholder-white" type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)}/>
+        </div>
+        <div className="mt-5 mb-6 w-1/2 border-2 border-solid border-white m-auto rounded-full">
+        <input className="block m-auto p-3 w-56 bg-transparent border-none outline-none placeholder-white" placeholder="Password" type="password" onChange={(e) => setPassword(e.target.value)}/>
+        </div>
+        <div className="m-auto w-96 flex justify-between mb-10 mt-8">
+        <button className="ml-3 border-2  border-collapse border-white rounded-full p-2 pl-4 pr-4 text-white hover:text-black hover:bg-gradient-to-r from-slate-100 to-slate-400" onClick={signIn}>Sign In</button>
+        <button className="border-2 border-solid border-white rounded-full p-2 pl-4 pr-4 text-white hover:text-black hover:bg-gradient-to-r from-slate-100 to-slate-400" onClick={signInWithGoogle}> Sign In With Google</button>
+        <button className="mr-3 border-2 borrder-solid border-white rounded-full p-2 pl-4 pr-4 text-white hover:text-black hover:bg-gradient-to-r from-slate-100 to-slate-400" onClick={Logout}>Logout</button>
+        </div>
         
     </>
 
